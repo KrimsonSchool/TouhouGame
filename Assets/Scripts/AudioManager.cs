95780b4d5b67fe691b47a786c008b3721f15b386
Kevin.V <1025532@student.sae.edu.au>
2025-11-17 11:56:55 +1000

    assets
    * added Assets of background
    * added music to be in the Music audio mix
    * made a new list of music for both levels and boss music

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -12,15 +12,7 @@ public class AudioManager : MonoBehaviour
     
     public Slider healthBar;
     public Slider shieldBar;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    
 }
